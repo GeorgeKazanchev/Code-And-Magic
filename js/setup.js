3e3f54e636a0105a4ee18f56d49f8e32cddc4b75
George Kazanchev <127313091+GeorgeKazanchev@users.noreply.github.com>
2024-12-04 07:02:34 +0300

    Реализованы сценарии пользовательского взаимодействия
    1. Открытие / закрытие окна настройки персонажа. Открытие может быть осуществлено кликом мышью или нажатием клавиши Enter на аватарке. Закрытие - кликом на крестик, нажатием клавиши Enter или клавиши Escape.
    
    2. Отправка формы с данными о персонаже путём нажатия кнопки "Сохранить".
    
    3. Валидация ввода имени персонажа.
    
    4. Изменение цветов мантии, глаз и файерболла персонажа при клике на них.

--- js/setup.js
+++ js/setup.js
@@ -1,13 +1,22 @@
 'use strict';
 
 var setupElement = document.querySelector('.setup');
-setupElement.classList.remove('hidden');
+var setupOpenElement = document.querySelector('.setup-open');
+var setupCloseElement = document.querySelector('.setup-close');
+var setupOpenIconElement = document.querySelector('.setup-open-icon');
 
 var similarElement = document.querySelector('.setup-similar');
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
   .content
   .querySelector('.setup-similar-item');
+
+var setupCoatElement = document.querySelector('.setup-wizard .wizard-coat');
+var setupEyesElement = document.querySelector('.setup-wizard .wizard-eyes');
+var setupFireballElement = document.querySelector('.setup-fireball-wrap');
+var setupCoatInput = document.querySelector('input[name="coat-color"]');
+var setupEyesInput = document.querySelector('input[name="eyes-color"]');
+var setupFireballInput = document.querySelector('input[name="fireball-color"]');
 
 var WIZARDS_COUNT = 4;
 
@@ -43,11 +52,19 @@ var COAT_COLORS = [
 ];
 
 var EYES_COLORS = [
-  'black',
-  'red',
-  'blue',
-  'yellow',
-  'green',
+  'rgb(0, 0, 0)',
+  'rgb(255, 0, 0)',
+  'rgb(0, 0, 255)',
+  'rgb(255, 255, 0)',
+  'rgb(0, 255, 0)',
+];
+
+var FIREBALL_COLORS = [
+  'rgb(238, 72, 48)',
+  'rgb(48, 168, 238)',
+  'rgb(92, 230, 192)',
+  'rgb(232, 72, 213)',
+  'rgb(230, 232, 72)',
 ];
 
 var getRandomArrayItem = function(arr) {
@@ -75,6 +92,74 @@ var renderWizard = function(wizard) {
 
   return wizardElement;
 };
+
+var selectNextColor = function(colorsArray, currentColor) {
+  var currentColorIndex = colorsArray.indexOf(currentColor);
+  return (currentColorIndex >= 0 && currentColorIndex < colorsArray.length - 1)
+    ? colorsArray[currentColorIndex + 1]
+    : colorsArray[0];
+};
+
+var popupEscPressHandler = function(evt) {
+  if (evt.key === 'Escape') {
+    if (evt.target.matches('.setup-user-name')) {
+      return;
+    }
+
+    closePopup();
+  }
+};
+
+var openPopup = function() {
+  setupElement.classList.remove('hidden');
+  document.addEventListener('keydown', popupEscPressHandler);
+};
+
+var closePopup = function() {
+  setupElement.classList.add('hidden');
+  document.removeEventListener('keydown', popupEscPressHandler);
+};
+
+setupOpenElement.addEventListener('click', function() {
+  openPopup();
+});
+
+setupOpenIconElement.addEventListener('keydown', function(evt) {
+  if (evt.key === 'Enter') {
+    openPopup();
+  }
+});
+
+setupCloseElement.addEventListener('click', function() {
+  closePopup();
+});
+
+setupCloseElement.addEventListener('keydown', function(evt) {
+  if (evt.key === 'Enter') {
+    closePopup();
+  }
+});
+
+setupCoatElement.addEventListener('click', function(evt) {
+  var target = evt.target;
+  var currentColor = target.style.fill;
+  target.style.fill = selectNextColor(COAT_COLORS, currentColor);
+  setupCoatInput.value = target.style.fill;
+});
+
+setupEyesElement.addEventListener('click', function(evt) {
+  var target = evt.target;
+  var currentColor = target.style.fill;
+  target.style.fill = selectNextColor(EYES_COLORS, currentColor);
+  setupEyesInput.value = target.style.fill;
+});
+
+setupFireballElement.addEventListener('click', function(evt) {
+  var target = evt.currentTarget;
+  var currentColor = target.style.backgroundColor;
+  target.style.backgroundColor = selectNextColor(FIREBALL_COLORS, currentColor);
+  setupFireballInput.value = target.style.backgroundColor;
+});
 
 var wizards = generateWizards();
 
